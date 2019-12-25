49f5982b8b73eca1c68b6ce8b1a065da3a3afd1e
nicozar95 <nzarewsky@gmail.com>
2019-12-25 20:47:54 -0300

    new;

--- bot.rb
+++ bot.rb
@@ -14,7 +14,7 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      scheduler = Rufus::Scheduler.start_new
+      scheduler = Rufus::Scheduler.new
       scheduler.every '10s' do
         response = HTTParty.get('http://inspirobot.me/api?generate=true')
         bot.api.send_photo(chat_id: message.chat.id, photo: response.body)
