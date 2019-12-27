ae6505e7c04f22606c94a74057e4ef52c30d3084
nicozar95 <nzarewsky@gmail.com>
2019-12-27 15:19:39 -0300

    fox

--- bot.rb
+++ bot.rb
@@ -10,7 +10,7 @@ Dotenv.load
 
 token = ENV['TELEGRAM_TOKEN']
 
-PHRASES = ["es una mierda", "me quiero ir", "malardo", "buenardo", "q haces mi rey?", "ya fue hago, home"]
+PHRASES = ["es una mierda", "me quiero ir", "malardo", "buenardo", "q haces mi rey?", "ya fue, hago home"]
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
