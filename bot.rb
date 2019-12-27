416489ae8a962847cfbe1595f47cfb9a50bf5573
nicozar95 <nzarewsky@gmail.com>
2019-12-27 15:09:42 -0300

    fix

--- bot.rb
+++ bot.rb
@@ -10,12 +10,16 @@ Dotenv.load
 
 token = ENV['TELEGRAM_TOKEN']
 
+PHRASES = ["es una mierda"]
+
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when /mi rey/i
       response = HTTParty.get('http://inspirobot.me/api?generate=true')
       bot.api.send_photo(chat_id: message.chat.id, photo: response.body)
+    when /que diria joaco/i
+      bot.api.send_message(chat_id: message.chat.id, text: PHRASES.sample)
     end
   end
 end
