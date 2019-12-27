86c554b93a2b121d71655e37435dac88b7a8217e
nicozar95 <nzarewsky@gmail.com>
2019-12-27 15:47:27 -0300

    fix

--- bot.rb
+++ bot.rb
@@ -13,7 +13,7 @@ token = ENV['TELEGRAM_TOKEN']
 PHRASES = ["es una mierda", "me quiero ir", "malardo", "buenardo",
            "q haces mi rey?", "ya fue, hago home", "arre", "same", "q paja",
            "ya fue, renuncio", "LOCO NO ME DAN PERMISOS!", "me voy a fumar un pucho",
-           "QUE PAJA ESTE PROYECTOOOOOOoooooOoooO!"]
+           "QUE PAJA ESTE PROYECTOOOOOOoooooOoooO!", "josena que opinas?"]
 
 PEOPLE = ["paquito amoroso", "leon", "truenito", "nicki nicole","bokita el mas grande papa"]
 
