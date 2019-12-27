668ea29fcc34eb86dcc67e1d0e76038557a2a5c8
nicozar95 <nzarewsky@gmail.com>
2019-12-27 13:08:12 -0300

    Fix?

--- bot.rb
+++ bot.rb
@@ -14,10 +14,12 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      scheduler = Rufus::Scheduler.new
-      scheduler.cron '0 16 * * *' do
-        response = HTTParty.get('http://inspirobot.me/api?generate=true')
-        bot.api.send_photo(chat_id: message.chat.id, photo: response.body)
+      loop do
+        scheduler = Rufus::Scheduler.new
+        scheduler.cron '0 16 * * *' do
+          response = HTTParty.get('http://inspirobot.me/api?generate=true')
+          bot.api.send_photo(chat_id: message.chat.id, photo: response.body)
+        end
       end
     end
   end
